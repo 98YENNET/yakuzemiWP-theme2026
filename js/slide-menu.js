3bdd0c0dabbf59e5ddb9a7cf3567cc0bfe34959d
WEELOCH <info@weeloch.co.jp>
2026-05-28 23:23:54 +0900

    fix: SPメニュー第2階層展開時に第2階層が一瞬閉じる問題を修正（stopPropagationで階層バブリングを遮断）

--- js/slide-menu.js
+++ js/slide-menu.js
@@ -1,15 +1,21 @@
 $(document).ready(function () {
-    //初期状態では隠す
+    // 初期状態では隠す
     $('#accordion_box ul').hide();
-    //親をクリックしたら
-    $('.main_trigger').click(function () {
-        //クリックされた子供のｕｌを表示する
+
+    // 第1階層トリガー
+    // stopPropagation で上位へのバブリングを止める
+    $('#accordion_box > li.main_trigger').click(function (e) {
+        e.stopPropagation();
         $(this).children('ul').slideDown(300);
-        //それ以外のulは隠す
-        $('#accordion_box  > li >ul').not($(this).children('ul')).slideUp(300);
+        $('#accordion_box > li.main_trigger').not(this).children('ul').slideUp(300);
     });
-    $('#accordion_box > li > ul > li').click(function () {
+
+    // 第2階層トリガー
+    // stopPropagation で第1階層への誤ったバブリングを止める
+    // （バブリングすると第2階層が一瞬閉じようとする）
+    $('#accordion_box > li > ul > li.main_trigger').click(function (e) {
+        e.stopPropagation();
         $(this).children('ul').slideDown(300);
-        $('#accordion_box > li > ul > li > ul').not($(this).children('ul')).slideUp(300);
+        $(this).siblings('li.main_trigger').children('ul').slideUp(300);
     });
 });
