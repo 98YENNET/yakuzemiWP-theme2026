f395da61417e852c157a8463b7a04f23a1f2c376
WEELOCH <info@weeloch.co.jp>
2026-05-28 23:21:13 +0900

    fix: SPメニューのサブメニュー開閉をslideDown/Upに変更（左上→右下アニメーションを廃止）

--- js/slide-menu.js
+++ js/slide-menu.js
@@ -4,12 +4,12 @@ $(document).ready(function () {
     //親をクリックしたら
     $('.main_trigger').click(function () {
         //クリックされた子供のｕｌを表示する
-        $(this).children('ul').show('600');
+        $(this).children('ul').slideDown(300);
         //それ以外のulは隠す
-        $('#accordion_box  > li >ul').not($(this).children('ul')).hide('600');
+        $('#accordion_box  > li >ul').not($(this).children('ul')).slideUp(300);
     });
     $('#accordion_box > li > ul > li').click(function () {
-        $(this).children('ul').show('600');
-        $('#accordion_box > li > ul > li > ul').not($(this).children('ul')).hide('600');
+        $(this).children('ul').slideDown(300);
+        $('#accordion_box > li > ul > li > ul').not($(this).children('ul')).slideUp(300);
     });
 });
