b67b9b2d80da810fcebbbf17bad691d072cf6dc6
WEELOCH <info@weeloch.co.jp>
2026-05-28 23:27:41 +0900

    feat: SPメニューのアコーディオンにトグル閉じ機能と.fa-angle-down 180度回転を追加

--- js/slide-menu.js
+++ js/slide-menu.js
@@ -3,19 +3,24 @@ $(document).ready(function () {
     $('#accordion_box ul').hide();
 
     // 第1階層トリガー
-    // stopPropagation で上位へのバブリングを止める
     $('#accordion_box > li.main_trigger').click(function (e) {
         e.stopPropagation();
-        $(this).children('ul').slideDown(300);
-        $('#accordion_box > li.main_trigger').not(this).children('ul').slideUp(300);
+        $(this).children('ul').slideToggle(300);
+        $(this).toggleClass('is-open');
+        // 他の第1階層を閉じてアイコンをリセット
+        $('#accordion_box > li.main_trigger').not(this)
+            .children('ul').slideUp(300)
+            .end().removeClass('is-open');
     });
 
     // 第2階層トリガー
-    // stopPropagation で第1階層への誤ったバブリングを止める
-    // （バブリングすると第2階層が一瞬閉じようとする）
     $('#accordion_box > li > ul > li.main_trigger').click(function (e) {
         e.stopPropagation();
-        $(this).children('ul').slideDown(300);
-        $(this).siblings('li.main_trigger').children('ul').slideUp(300);
+        $(this).children('ul').slideToggle(300);
+        $(this).toggleClass('is-open');
+        // 他の第2階層を閉じてアイコンをリセット
+        $(this).siblings('li.main_trigger')
+            .children('ul').slideUp(300)
+            .end().removeClass('is-open');
     });
 });
