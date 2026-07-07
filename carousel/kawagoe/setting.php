bd7be3ba3df2c58ee75179476cad46bf05b18c08
WEELOCH <info@weeloch.co.jp>
2026-07-07 18:21:16 +0900

    chore: 死蔵ファイル・ディレクトリを削除
    WPのテンプレート階層に一致しないバックアップ/日付付きファイル、
    include/配下の重複コピー、carousel/include.phpから参照されない
    carousel/kawagoe/一式・テスト用include_test.phpを削除。
    page-feed240111.php・page-supporters2023.phpはDB上に該当スラッグの
    固定ページが存在しないことを確認済み

--- carousel/kawagoe/setting.php
+++
@@ -1,9 +0,0 @@
-<?php
-$classImages = array(
-    ["main_01_sp.jpg", "テキストテキスト"],
-    ["topics-recruit.jpg"],
-    ["top_mainimage1.jpg", "PCと同じポップアップもしくは、ポップアップなしの横スライドのみ<br />（10枚目の次は1枚目としてループ"],
-    ["top_1.png", "テキスト3333テキスト"],
-);
-
-
